f8196d5f0f7e3fdb9f19c84405f45288c5cf044f
monophoto <jetsadawut1013@gmail.com>
2023-07-18 04:10:47 +0700

    edit-fullname

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = (props) => {
   return (
     <div className="mt-3">
       <p className="text-secondary text-center">
-        copyright © {props.year} {pops.fullName} {props.studentId}
+        copyright © {props.year} {props.fullName} {props.studentId}
       </p>
     </div>
   );
